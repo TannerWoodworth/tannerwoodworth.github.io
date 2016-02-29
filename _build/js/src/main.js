df1401be312ee2221fc85868841384376b3d17c9
TannerWoodworth <twoodworth2014@gmail.com>
2016-02-28 22:38:06 -0500

    -

--- _build/js/src/main.js
+++ _build/js/src/main.js
@@ -2,23 +2,32 @@ console.log("main.js success");
 var showing = false;
 function expandNav (e){
   event.preventDefault();
-  console.log(showing);
   if (showing == true){
-    console.log("was true")
     document.getElementsByTagName("nav")[0].setAttribute("class", "hidden");
     showing = false;
   } else if (showing == false){
-    console.log("was false")
     document.getElementsByTagName("nav")[0].setAttribute("class", "show");
     showing = true;
   };
 };
 function showBuild (e){
-
+  console.log("build");
+  document.getElementById('pg-home').setAttribute("class", "hide-section");
+  document.getElementById('pg-about').setAttribute("class", "hide-section");
+  document.getElementById('pg-cart').setAttribute("class", "hide-section");
+  document.getElementById('pg-build').setAttribute("class", "show-section");
 }
 function showAbout (e){
-
+  console.log("about");
+  document.getElementById('pg-home').setAttribute("class", "hide-section");
+  document.getElementById('pg-build').setAttribute("class", "hide-section");
+  document.getElementById('pg-cart').setAttribute("class", "hide-section");
+  document.getElementById('pg-about').setAttribute("class", "show-section");
 }
 function showCart (e){
-
+  console.log("cart");
+  document.getElementById('pg-home').setAttribute("class", "hide-section");
+  document.getElementById('pg-about').setAttribute("class", "hide-section");
+  document.getElementById('pg-build').setAttribute("class", "hide-section");
+  document.getElementById('pg-cart').setAttribute("class", "show-section");
 }
